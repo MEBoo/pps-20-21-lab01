17c5019bd6c0cb42e23f942ceb55724354563136
MEBoo <39593253+MEBoo@users.noreply.github.com>
2021-03-06 11:30:34 +0100

    CircularListTest first setup/test + added CirclularListImpl

--- pps-lab01-tdd/test/CircularListTest.java
+++ pps-lab01-tdd/test/CircularListTest.java
@@ -1,17 +1,27 @@
+import lab01.tdd.CircularList;
+import lab01.tdd.CircularListImpl;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * The test suite for testing the CircularList implementation
  */
 public class CircularListTest {
 
-    //TODO: test implementation
-
-    @Disabled
-    @Test public void testTodo(){
-        Assertions.fail();
+    private CircularList circularList;
+    
+    @BeforeEach
+    public void setup() {
+        this.circularList = new CircularListImpl();
     }
+
+    @Test
+    public void testInitialSize() {
+        assertEquals(0, circularList.size());
+    }
+
 
 }
